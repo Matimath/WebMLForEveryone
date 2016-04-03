31553861a55aef6c02d454914e0de0ffccb3d5e7
Michal Preibisch <mp347207@students.mimuw.edu.pl>
2016-04-03 21:12:08 +0200

    Drobne zmiany

--- WebMLForEveryone/upload/admin.py
+++ WebMLForEveryone/upload/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import UserFile
+from .models import MachineLearningObject
+
+admin.site.register(UserFile)
+
+admin.site.register(MachineLearningObject)
--- WebMLForEveryone/upload/models.py
+++ WebMLForEveryone/upload/models.py
@@ -5,12 +5,14 @@ class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
 
 
-# Pierwsza linia w Pythonie :D
-class File(models.Model):
+# Tabelka na Excelowe pliki uzytkownikow.
+class UserFile(models.Model):
     file_id = models.AutoField(primary_key=True)
     hash = models.CharField(max_length=20)
     upload = models.FileField(upload_to='uploads/user_files/%Y/%m/%d/')
 
-class MLobject(models.Model):
+
+# Obiekt do machine learninug
+class MachineLearningObject(models.Model):
     object_id = models.AutoField(primary_key=True)
     upload = models.FileField(upload_to='uploads/ML_object/%Y/%m/%d/')
